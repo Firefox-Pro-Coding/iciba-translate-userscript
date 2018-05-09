f2c1f9c968d6498c3f48be6247ecc05398f85ad3
noe132 <nnnnoe132@gmail.com>
2018-05-09 10:27:15 +0800

    svg hash update

--- utils/svg-hash-loader.js
+++ utils/svg-hash-loader.js
@@ -5,7 +5,8 @@ module.exports = function icibahtmlLoader(source) {
   const header = 'data:image/svg+xml;base64,'
   const base64Data = Buffer.from(source).toString('base64')
   const data = `${header}${base64Data}`
-  const hash = crypto.createHash('sha256').update('alice', 'utf8').digest('hex')
+  // 5 bytes should be enough
+  const hash = crypto.createHash('sha256').update(source).digest('hex').slice(0, 10)
   const result = `module.exports = {
     hash: ${JSON.stringify(hash)},
     data: ${JSON.stringify(data)},
