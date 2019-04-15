9982e96c64e2323274bc94bc2552b411911f5d52
noe132 <nnnnoe132@gmail.com>
2019-04-15 18:21:27 +0800

    fix: build script

--- utils/addMetaData.ts
+++ utils/addMetaData.ts
@@ -8,6 +8,6 @@ const packageInfo = require('../package.json')
 const metaData = fs.readFileSync(path.join(__dirname, '../config/metaData.js'))
   .toString()
   .replace('{{ version }}', `${packageInfo.version}`)
-const indexJs = fs.readFileSync(path.join(__dirname, '../dist/index.js')).toString()
+const indexJs = fs.readFileSync(path.join(__dirname, '../dist/iciba.user.js')).toString()
 
-fs.writeFileSync(path.join(__dirname, '../dist/index.js'), `${metaData}\n${indexJs}`)
+fs.writeFileSync(path.join(__dirname, '../dist/iciba.user.js'), `${metaData}\n${indexJs}`)
