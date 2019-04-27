016c316820f7733661bce6a305469db89279d48d
noe132 <nnnnoe132@gmail.com>
2019-04-27 20:04:57 +0800

    feat: add shadow dom v0 support (#8)

--- src/createRoot.ts
+++ src/createRoot.ts
@@ -3,7 +3,20 @@ import Vue from 'vue'
 const icibaRoot = document.createElement('div')
 icibaRoot.className = 'iciba-root'
 document.body.appendChild(icibaRoot)
-const shadowRoot = icibaRoot.attachShadow({ mode: 'open' })
+
+const shadowRoot = (() => {
+  let sr: ShadowRoot
+
+  if (icibaRoot.attachShadow) {
+    sr = icibaRoot.attachShadow({ mode: 'open' })
+  } else {
+    // shadow dom v0
+    // < firefox 59
+    sr = (icibaRoot as any).createShadowRoot()
+  }
+
+  return sr
+})()
 
 Vue.prototype.shadowRoot = shadowRoot
 Vue.prototype.icibaRoot = icibaRoot
