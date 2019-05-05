e2a9e3af89792f592ac3e08df2d0ddddab0d9224
noe132 <nnnnoe132@gmail.com>
2019-05-05 18:53:57 +0800

    chord: upgrade to corejs@3

--- config/babelrc.js
+++ config/babelrc.js
@@ -2,13 +2,17 @@ module.exports = () => ({
   'presets': [
     '@vue/babel-preset-jsx',
     ['@babel/preset-env', {
+      targets: {
+        chrome: '53',
+        firefox: '29',
+      },
       modules: false,
       useBuiltIns: false,
     }],
   ],
   'plugins': [
     ['@babel/transform-runtime', {
-      corejs: 2,
+      corejs: 3,
       useESModules: true,
     }],
   ],
