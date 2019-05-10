221843cd17c25c61e0d6ece7540c36517f9338e2
noe132 <nnnnoe132@gmail.com>
2019-05-10 20:10:16 +0800

    fix: checkbox width

--- src/components/SettingPage/components/checkbox/checkbox.vue
+++ src/components/SettingPage/components/checkbox/checkbox.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="flex align-center checkbox" @click="toggle">
-    <div class="icon" :class="{ checked: value }">
-      <i-icon size="100%" :svg="value ? icon.checked : icon.unchecked" />
-      <div v-ripple="{ center: true }" class="ripple" :class="{ checked: value }">
-        <div class="ripple-trigger"></div>
+  <div class="flex align-center checkbox">
+    <div class="flex check-box-wrapper" @click="toggle">
+      <div class="icon" :class="{ checked: value }">
+        <i-icon size="100%" :svg="value ? icon.checked : icon.unchecked" />
+        <div v-ripple="{ center: true }" class="ripple" :class="{ checked: value }">
+          <div class="ripple-trigger"></div>
+        </div>
       </div>
+      <div class="label grey--text text--darken-2">{{ label }}</div>
     </div>
-    <div class="label grey--text text--darken-2">{{ label }}</div>
   </div>
 </template>
 
--- src/components/SettingPage/components/radio/radio.vue
+++ src/components/SettingPage/components/radio/radio.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="flex align-center flex-nogrow radio mr-3 mt-1 mb-1" @click="handleClick">
-    <div class="icon" :class="{ checked }">
-      <i-icon size="100%" :svg="checked ? icon.checked : icon.unchecked" />
-      <div v-ripple="{ center: true }" class="ripple" :class="{ checked }">
-        <div class="ripple-trigger"></div>
+  <div class="flex align-center flex-nogrow radio mr-3 mt-1 mb-1">
+    <div class="flex radio-wrapper" @click="handleClick">
+      <div class="icon" :class="{ checked }">
+        <i-icon size="100%" :svg="checked ? icon.checked : icon.unchecked" />
+        <div v-ripple="{ center: true }" class="ripple" :class="{ checked }">
+          <div class="ripple-trigger"></div>
+        </div>
       </div>
+      <div class="label">{{ label }}</div>
     </div>
-    <div class="label">{{ label }}</div>
   </div>
 </template>
 
