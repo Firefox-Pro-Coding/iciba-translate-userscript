7fd0dd780bfa21c65eb0ee4d9a670e3b273bd0ca
noe132 <nnnnoe132@gmail.com>
2019-08-28 19:31:52 +0800

    fix: sougou translate key (#18)

--- src/provider/SougouTranslate/getKey.ts
+++ src/provider/SougouTranslate/getKey.ts
@@ -3,26 +3,25 @@ import { got } from '~/util/gmapi'
 const getKey = async () => {
   const sougouFanyiRes = await got({
     method: 'GET',
-    url: 'https://fanyi.sogou.com/',
+    url: 'https://fanyi.sogou.com/logtrace',
+    headers: {
+      'Accept': '*/*',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'DNT': '1',
+      'Host': 'fanyi.sogou.com',
+      'Pragma': 'no-cache',
+      'Referer': 'https://fanyi.sogou.com/',
+      // 'Sec-Fetch-Mode': 'no-cors',
+      // 'Sec-Fetch-Site': 'same-origin',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+    },
     timeout: 5000,
   })
-  const jsUrlMatch = sougouFanyiRes.responseText.match(/\/\/(dlweb\.sogoucdn\.com\/translate\/pc\/static\/js\/app\.[a-z0-9]{4,12}\.js)/)
-  if (!jsUrlMatch) {
+  const seccodeMatch = sougouFanyiRes.responseText.match(/'([a-f0-9]+)'\+'([a-f0-9]+)'/)
+  if (!seccodeMatch) {
     throw new Error('解析出错！')
   }
-  const jsUrl = jsUrlMatch[1]
-
-  const jsRes = await got({
-    method: 'GET',
-    url: `https://${jsUrl}`,
-    timeout: 5000,
-  })
-  const keyMatch = jsRes.responseText.match(/"([a-z0-9]{32})"/)
-  if (!keyMatch) {
-    throw new Error('解析出错！')
-  }
-  const key = keyMatch[1]
-  return key
+  return seccodeMatch[1] + seccodeMatch[2]
 }
 
 export default getKey
