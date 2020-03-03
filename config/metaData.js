08fb4f004d555aa8686c53ec7883c4176d038e1e
noe132 <nnnnoe132@gmail.com>
2020-03-03 21:55:37 +0800

    chord: update description

--- config/metaData.js
+++ config/metaData.js
@@ -15,7 +15,7 @@
 // @icon               http://tb.himg.baidu.com/sys/portrait/item/d4346e6f65313332ac06
 // @version            {{ version }}
 // @supportURL         https://github.com/Firefox-Pro-Coding/iciba-translate-userscript
-// @description        简单好用的划词翻译。支持查词或翻译。支持iciba，google字典，google翻译，百度翻译等接口
+// @description        简单好用的划词查词翻译。支持iciba，google字典，google翻译，百度翻译等多种接口
 // ==/UserScript==
 
 // this file was compiled by webpack.
