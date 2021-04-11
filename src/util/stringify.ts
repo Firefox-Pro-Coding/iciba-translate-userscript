b9993d0164a7316af8cf0261949f1181a1443803
noe132 <nnnnoe132@gmail.com>
2021-04-11 22:16:34 +0800

    fix: Array.prototype.toJSON key polution

--- src/util/stringify.ts
+++ src/util/stringify.ts
@@ -1,9 +1,14 @@
 /** fix prototype.js Array.prototype.toJSON pollution */
 export const stringify = (...args: Parameters<typeof JSON.stringify>) => {
+  const stringyfy = JSON.stringify
   const arrayToJSON = (Array.prototype as any).toJSON
-  const stringyfy = JSON.stringify;
-  (Array.prototype as any).toJSON = undefined
-  const result = stringyfy.call(JSON, ...args);
-  (Array.prototype as any).toJSON = arrayToJSON
+  const hasToJSON = 'toJSON' in Array.prototype
+  if (hasToJSON) {
+    (Array.prototype as any).toJSON = undefined
+  }
+  const result = stringyfy.call(JSON, ...args)
+  if (hasToJSON) {
+    (Array.prototype as any).toJSON = arrayToJSON
+  }
   return result
 }
