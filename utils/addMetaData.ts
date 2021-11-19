e39084f5c24cb8ce1b804d57eb13bd076d76f3b8
noe132 <nnnnoe132@gmail.com>
2021-11-19 12:29:05 +0800

    fix: revert promise to native promise #53

--- utils/addMetaData.ts
+++ utils/addMetaData.ts
@@ -15,9 +15,17 @@ const scriptContent = fs.readFileSync(path.join(__dirname, '../dist/iciba.user.j
   // TEMP FIX: remove sourcemapping from base64-arraybuffer
   .replace(/\n\/\/# sourceMappingURL=.{1,100}\n/, '\n')
 
+// #53 Promise was overridden
+const inits = `
+var Promise = Reflect.getPrototypeOf(Response.prototype.text().catch(() => {})).constructor;
+window.Promise = Promise;
+var self = window;
+`
+
 const content = [
   metaData,
   '\n',
+  inits,
   'if (typeof GM !== \'undefined\' || typeof GM_setValue !== \'undefined\') {\n',
   scriptContent,
   '\n}',
