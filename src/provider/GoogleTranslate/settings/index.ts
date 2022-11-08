9f70514d73ef673dd6aa03782277a6d13dc64abf
lieyanqzu <lieyanqzu@hotmail.com>
2022-11-08 09:55:54 +0800

    rm: remove deprecated google.cn api domain

--- src/provider/GoogleTranslate/settings/index.ts
+++ src/provider/GoogleTranslate/settings/index.ts
@@ -2,10 +2,6 @@ import { defineComponent, watch, onUnmounted } from 'vue'
 
 import { googleLanguagesOptions, GOOGLE_LANGUAGES } from '~/provider/GoogleTranslate/googleLanguages'
 import ProviderCommon from '~/components/providerCommon/providerCommon.vue'
-import {
-  GOOGLE_TRANSLATE_HOST,
-  GOOGLE_TRANSLATE_HOST_MAP,
-} from '../constant'
 import { store } from '../store'
 import { icons } from '../icons'
 
@@ -15,11 +11,6 @@ const iconOptions = Object
     icon: v,
     key: k,
   }))
-
-const hostOptions = [
-  { label: GOOGLE_TRANSLATE_HOST_MAP[GOOGLE_TRANSLATE_HOST.GOOGLE_COM], key: GOOGLE_TRANSLATE_HOST.GOOGLE_COM },
-  { label: GOOGLE_TRANSLATE_HOST_MAP[GOOGLE_TRANSLATE_HOST.GOOGLE_CN], key: GOOGLE_TRANSLATE_HOST.GOOGLE_CN },
-]
 
 export default defineComponent({
   name: 'GoogleTranslateSettings',
@@ -55,7 +46,6 @@ export default defineComponent({
     return {
       form,
       iconOptions,
-      hostOptions,
       languageOptions: googleLanguagesOptions,
     }
   },
